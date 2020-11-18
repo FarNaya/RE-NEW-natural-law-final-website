454a87db0e734d30ee70e67ad4c6a136b7261280
FarNaya <56936820+FarNaya@users.noreply.github.com>
2020-11-18 13:42:31 -0700

    upload files

---
+++ js/slide.js
@@ -0,0 +1,23 @@
+$('.slider').slick({
+  draggable: true,
+  arrows: false,
+  dots: false,
+  fade: true,
+  speed: 700,
+  autoplay:true,
+  infinite: true,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  touchThreshold: 100
+})
+
+$('.about-slider').slick({
+  draggable: true,
+  arrows: false,
+  dots: false,
+  fade: true,
+  speed: 700,
+  autoplay:true,
+  infinite: true,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  touchThreshold: 100
+})
